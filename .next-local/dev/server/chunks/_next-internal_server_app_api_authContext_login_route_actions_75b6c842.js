393f131c087a0f343372f8d0bb156c7887e4aa79
MalickDevWeb <malickteuwdevweb@gmail.com>
2026-03-05 06:20:57 +0000

    chore(gitignore): ignorer .next-local et tmp + retirer artefacts du repo

--- .next-local/dev/server/chunks/_next-internal_server_app_api_authContext_login_route_actions_75b6c842.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/authContext/login/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_authContext_login_route_actions_75b6c842.js.map
--- .next-local/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IG9QHxanp1StjYUyiVopdNtN4HaZ08tkLZEqEkPPiow=\"\n}"
